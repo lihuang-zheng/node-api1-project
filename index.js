954c3f1f8e5fd96a59a453a5bb7c1dc01a665ce4
lihuang-zheng <jerryzheng0131@gmail.com>
2020-03-09 17:17:36 -0500

    added get with :id.

--- index.js
+++ index.js
@@ -17,6 +17,19 @@ server.get("/api/users", (req, res) => {
   res.status(200).json(users);
 });
 
+// // GET ID
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.find(user => user.id === String(id));
+  if (!user) {
+    res.status(404).json({ message: "user with given id does not exist" });
+  } else if (!id) {
+    res.status(500).json({ message: "server error" });
+  } else {
+    res.status(200).json(user);
+  }
+});
+
 // POST
 server.post("/api/users", (req, res) => {
   const userInfo = req.body;
@@ -39,10 +52,6 @@ server.post("/api/users", (req, res) => {
   }
 });
 
-// DELETE
-server.delete("/api/users/:id", (req, res) => {
-  res.status(200);
-});
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const PORT = 5000;
